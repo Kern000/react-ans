5d5823c39675ba4aa2fe9814edf11978e4621848
Kern000 <holupkern@gmail.com>
2024-04-03 10:47:04 +0800

    completed qn 13

--- 13-add-numbers/13-add-numbers/src/Numbers.js
+++ 13-add-numbers/13-add-numbers/src/Numbers.js
@@ -3,21 +3,18 @@ import React, {useState} from 'react'
 export default function Numbers () {
   
     const [allNumbers, setAllNumbers] = useState([1,3,5,7]);
-  
+    const [newNumber, setNewNumber] = useState(0);
 
     const addRandom = ()=> {
-        // 1. clone the allNumbers array in the state
 
-        // 2. add a random number to the back of the clone
-        let randomNumber = Math.floor(Math.random() * 10 + 1)
+        let randomNumber = Math.floor(Math.random() * 10 + 1);
+        setAllNumbers([...allNumbers, randomNumber]);
 
-        // 3. set the cloned array back into the state
     }
 
     const addNumber = () => {
-        
+        setAllNumbers([...allNumbers, newNumber]);        
     }
-
 
     return <React.Fragment>
         <ul>
@@ -28,8 +25,8 @@ export default function Numbers () {
             <button onClick={addRandom}>Add Random</button>
         </div>
         <div>
-            <input type="text" name="newNumber"/>
-            <button>Add</button>
+            <input type="text" name="newNumber" value={newNumber} onChange={(event)=> setNewNumber(event.target.value)}/>
+            <button onClick={addNumber}>Add</button>
         </div>
     </React.Fragment>
 
